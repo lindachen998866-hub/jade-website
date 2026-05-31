7ff5dddeeb4c7c2310580f5a87e69b28875f0665
lindachen998866-hub <lindachen998866@gmail.com>
2026-05-31 15:14:23 +0800

    Delete sanity.js

--- sanity.js
+++
@@ -1,9 +0,0 @@
-// sanity.js
-import { createClient } from "https://esm.sh/@sanity/client";
-
-export const client = createClient({
-  projectId: "nuyprr23",  // 你的專案ID
-  dataset: "production",   // dataset
-  apiVersion: "2026-01-01",
-  useCdn: true,
-});
